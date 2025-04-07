033290a6ee4253899776960f4735d259ed52d117
abhisiroha <abhisheksiroha@gmail.com>
2025-04-07 17:50:54 +0000

    small font color change

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
     <div className='flex flex-row h-48 overflow-x-hidden bg-website-organge rounded-2xl md:px-6'>
 
       <div className='flex flex-col justify-start pt-4 px-4 gap-4'>
-        <h1 className='text-xl md:text-5xl'>raadhikamehtaa@gmail.com</h1>
+        <h1 className='text-xl md:text-5xl text-brown-font'>raadhikamehtaa@gmail.com</h1>
         <ContactList iconSrc="icons/linkedin.svg" iconText='Linkedin' link='https://www.linkedin.com/in/radhika-mehta-b25726171/'/>
         <ContactList/>
       </div>
